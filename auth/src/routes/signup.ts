3c5c2917d41b11a134720450c86d7b1eb6395088
Marcio T Rodrigues <marcio.rodrigues@boxti.com.br>
2023-03-19 12:09:58 -0300

    refactor: added common lib

--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -2,9 +2,8 @@ import express, {Request, Response} from 'express'
 import {body, validationResult} from 'express-validator'
 import jwt from 'jsonwebtoken'
 
-import { validateRequest } from "../middlewares/validate-request";
+import { validateRequest, BadRequestError } from "@zethtickets/common";
 import {User} from "../models/user";
-import {BadRequestError} from "../errors/bad-request-error";
 
 const router = express.Router()
 
